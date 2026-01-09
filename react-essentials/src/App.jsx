50a427f3770cce16ea63b4a123bbe8c4415e54f1
Paweł Pawiłojć <pawilojc@gmail.com>
2026-01-09 21:44:53 +0100

    refactor: update image imports and restructure CoreConcept component

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -1,3 +1,6 @@
+import reactImage from "./assets/react-core-concepts.png";
+import componentsImage from "./assets/components.png";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
@@ -8,7 +11,7 @@ function Header() {
     const description = reactDescriptions[genRandomInt(2)];
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+            <img src={reactImage} alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
                 {description} React concepts you will need for almost any app
@@ -18,12 +21,32 @@ function Header() {
     );
 }
 
+function CoreConcept(props) {
+    return (
+        <li>
+            <img src={props.image} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    );
+}
+
 function App() {
     return (
         <div>
             <Header></Header>
             <main>
-                <h2>Time to get started!</h2>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title="Components"
+                            description="The core UI building block."
+                            image={componentsImage}
+                        ></CoreConcept>
+                        <CoreConcept></CoreConcept>
+                    </ul>
+                </section>
             </main>
         </div>
     );
