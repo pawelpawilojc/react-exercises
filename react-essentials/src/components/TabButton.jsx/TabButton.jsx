95f9d919fb2b647696542e8b7303f2c4fe187992
Paweł Pawiłojć <pawilojc@gmail.com>
2026-01-10 18:16:23 +0100

    add Examples menu section with TabButton component

---
+++ react-essentials/src/components/TabButton.jsx/TabButton.jsx
@@ -0,0 +1,17 @@
+import "./TabButton.css";
+
+// export default function TabButton(props) {
+//     return (
+//         <li>
+//             <button>{props.children}</button>
+//         </li>
+//     );
+// }
+
+export default function TabButton({ children }) {
+    return (
+        <li>
+            <button>{children}</button>
+        </li>
+    );
+}
