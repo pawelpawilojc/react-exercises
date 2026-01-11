c000b6ea5abcb98e99abedc6bba2d600c365086b
Paweł Pawiłojć <pawilojc@gmail.com>
2026-01-11 15:44:28 +0100

    update TabButton component to handle selected state

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -48,16 +48,28 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton onSelect={() => handleSelect("components")}>
+                        <TabButton
+                            isSelected={selectedTopic === "components"}
+                            onSelect={() => handleSelect("components")}
+                        >
                             Components
                         </TabButton>
-                        <TabButton onSelect={() => handleSelect("jsx")}>
+                        <TabButton
+                            isSelected={selectedTopic === "jsx"}
+                            onSelect={() => handleSelect("jsx")}
+                        >
                             JSX
                         </TabButton>
-                        <TabButton onSelect={() => handleSelect("props")}>
+                        <TabButton
+                            isSelected={selectedTopic === "props"}
+                            onSelect={() => handleSelect("props")}
+                        >
                             Props
                         </TabButton>
-                        <TabButton onSelect={() => handleSelect("state")}>
+                        <TabButton
+                            isSelected={selectedTopic === "state"}
+                            onSelect={() => handleSelect("state")}
+                        >
                             State
                         </TabButton>
                     </menu>
