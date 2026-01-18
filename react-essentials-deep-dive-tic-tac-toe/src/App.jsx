9c0804e6b30e86ac692a7f74797fb6eb4678d5a8
Paweł Pawiłojć <pawilojc@gmail.com>
2026-01-18 10:12:01 +0100

    implement Player component with editing functionality

--- react-essentials-deep-dive-tic-tac-toe/src/App.jsx
+++ react-essentials-deep-dive-tic-tac-toe/src/App.jsx
@@ -1,10 +1,16 @@
-
+import Player from "./components/Player/Player.jsx";
 function App() {
-  
-
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <Player name="Player 1" symbol="X"></Player>
+                    <Player name="Player 2" symbol="O"></Player>
+                </ol>
+                GAME BOARD
+            </div>
+        </main>
+    );
 }
 
-export default App
+export default App;
