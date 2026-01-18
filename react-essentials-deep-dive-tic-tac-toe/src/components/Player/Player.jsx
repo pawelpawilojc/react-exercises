251284dc0be9f132f4b6fc53ab6a069e90f0739d
Paweł Pawiłojć <pawilojc@gmail.com>
2026-01-18 18:23:15 +0100

    enable editing functionality in Player component

--- react-essentials-deep-dive-tic-tac-toe/src/components/Player/Player.jsx
+++ react-essentials-deep-dive-tic-tac-toe/src/components/Player/Player.jsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
     function handleEditClick() {
-        setIsEditing(true);
+        setIsEditing((editing) => !editing);
     }
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    let buttonCaption = "Edit";
     if (isEditing) {
-        playerName = <input type="text" required></input>;
+        editablePlayerName = (
+            <input
+                type="text"
+                required
+                value={playerName}
+                onChange={handleChange}
+            ></input>
+        );
+        buttonCaption = "Save";
     }
+
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{buttonCaption}</button>
         </li>
     );
 }
