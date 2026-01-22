aa6affd81ee6c09cd2dfcb748ebc073c949f7ea4
Paweł Pawiłojć <pawilojc@gmail.com>
2026-01-22 20:24:37 +0100

    disable button when square is already occupied

--- react-essentials-deep-dive-tic-tac-toe/src/components/GameBoard/GameBoard.jsx
+++ react-essentials-deep-dive-tic-tac-toe/src/components/GameBoard/GameBoard.jsx
@@ -43,6 +43,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
                                     onClick={() =>
                                         onSelectSquare(rowIndex, colIndex)
                                     }
+                                    disabled={playerSymbol !== null}
                                 >
                                     {playerSymbol}
                                 </button>
