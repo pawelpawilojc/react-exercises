5442c1cb707ec51ede959ca044a3cdc3353dc59a
Paweł Pawiłojć <pawilojc@gmail.com>
2026-02-04 18:57:28 +0100

    update winner display

--- react-essentials-deep-dive-tic-tac-toe/src/App.jsx
+++ react-essentials-deep-dive-tic-tac-toe/src/App.jsx
@@ -21,6 +21,7 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+    const [players, setPlayers] = useState({ X: "Player 1", O: "Player 2" });
     const [gameTurns, setGameTurns] = useState([]);
     // const [hasWinner, setHasWinner] = useState(false);
     // const [activePlayer, setActivePlayer] = useState("X");
@@ -49,7 +50,7 @@ function App() {
             firstSquareSymbol === secondSquareSymbol &&
             firstSquareSymbol === thirdSquareSymbol
         ) {
-            winner = firstSquareSymbol;
+            winner = players[firstSquareSymbol];
         }
     }
 
@@ -73,6 +74,11 @@ function App() {
         setGameTurns([]);
     }
 
+    function handlePlayerNameChange(symbol, newName) {
+        setPlayers((prevPlayers) => {
+            return { ...prevPlayers, [symbol]: newName };
+        });
+    }
     return (
         <main>
             <div id="game-container">
@@ -81,11 +87,13 @@ function App() {
                         initialName="Player 1"
                         symbol="X"
                         isActive={activePlayer === "X"}
+                        onChangeName={handlePlayerNameChange}
                     ></Player>
                     <Player
                         initialName="Player 2"
                         symbol="O"
                         isActive={activePlayer === "O"}
+                        onChangeName={handlePlayerNameChange}
                     ></Player>
                 </ol>
                 {(winner || hasDraw) && (
