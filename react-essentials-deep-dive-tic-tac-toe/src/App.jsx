07ed32374ccab1ba04b0578e2e6706e40a66250a
Paweł Pawiłojć <pawilojc@gmail.com>
2026-02-04 18:36:17 +0100

    add GameOver component to display end screen

--- react-essentials-deep-dive-tic-tac-toe/src/App.jsx
+++ react-essentials-deep-dive-tic-tac-toe/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Player from "./components/Player/Player.jsx";
 import GameBoard from "./components/GameBoard/GameBoard.jsx";
 import Log from "./components/Log/Log.jsx";
+import GameOver from "./components/GameOver/GameOver.jsx";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
 const initialGameBoard = [
@@ -51,6 +52,8 @@ function App() {
         }
     }
 
+    const hasDraw = gameTurns.length === 9 && !winner;
+
     function handleSelectSquare(rowIndex, colIndex) {
         setGameTurns((prevTurns) => {
             const currentPlayer = deriveActivePlayer(prevTurns);
@@ -80,7 +83,7 @@ function App() {
                         isActive={activePlayer === "O"}
                     ></Player>
                 </ol>
-                {winner && <p>You won, {winner}!</p>}
+                {(winner || hasDraw) && <GameOver winner={winner} />}
                 <GameBoard
                     onSelectSquare={handleSelectSquare}
                     board={gameBoard}
