006fb02be9d773a5b2a3737833b3147c22c2cdc1
Paweł Pawiłojć <pawilojc@gmail.com>
2026-02-07 11:27:51 +0100

    add UserInput component

--- react-essentials-practice-project/src/App.jsx
+++ react-essentials-practice-project/src/App.jsx
@@ -1,6 +1,11 @@
 import Header from "./components/Header.jsx";
 function App() {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            <UserInput />
+        </>
+    );
 }
 
 export default App;
--- react-essentials-practice-project/src/components/Header.jsx
+++ react-essentials-practice-project/src/components/Header.jsx
@@ -2,8 +2,11 @@ import logo from "../assets/investment-calculator-logo.png";
 export default function Header() {
     return (
         <header id="header">
-            <img src={logo} alt="Logo showing a money bag"></img>
+            <img src={logo} alt="Logo showing a money bag" />
             <h1>Investment Calculator</h1>
+            <progress id="percent-loaded" value="75" max="100">
+                75 %
+            </progress>
         </header>
     );
 }
