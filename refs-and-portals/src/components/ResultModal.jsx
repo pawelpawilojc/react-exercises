7fd05054c04c99ea708abf78ce2515fccbb752a3
Paweł Pawiłojć <pawilojc@gmail.com>
2026-03-18 17:58:16 +0100

    refactor ResultModal and TimerChallenge to enhance result display and reset functionality

--- refs-and-portals/src/components/ResultModal.jsx
+++ refs-and-portals/src/components/ResultModal.jsx
@@ -1,6 +1,15 @@
 import { useImperativeHandle, useRef } from "react";
-export default function ResultModal({ ref, result, targetTime }) {
+export default function ResultModal({
+    ref,
+    targetTime,
+    remainingTime,
+    onReset,
+}) {
     const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
         return {
@@ -11,15 +20,16 @@ export default function ResultModal({ ref, result, targetTime }) {
     });
 
     return (
-        <dialog ref={dialog} className="result-modal">
-            <h2>You {result}</h2>
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
+            {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2>}
             <p>
                 The target time was <strong>{targetTime} seconds.</strong>
             </p>
             <p>
-                You stopped the timer with <strong>X seconds left.</strong>
+                <strong>{formattedRemainingTime} seconds left.</strong>
             </p>
-            <form method="dialog">
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
