6c6516eb76c803bab8de127914b14b2db102d1d9
Paweł Pawiłojć <pawilojc@gmail.com>
2026-07-11 16:59:20 +0200

    initialize pickedPlaces with stored selections from localStorage

--- side-effects/src/App.jsx
+++ side-effects/src/App.jsx
@@ -7,11 +7,16 @@ import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import { sortPlacesByDistance } from "./loc.js";
 
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIds.map((id) =>
+    AVAILABLE_PLACES.find((place) => place.id === id),
+);
+
 function App() {
     const modal = useRef();
     const selectedPlace = useRef();
     const [availablePlaces, setAvailablePlaces] = useState([]);
-    const [pickedPlaces, setPickedPlaces] = useState([]);
+    const [pickedPlaces, setPickedPlaces] = useState([storedPlaces]);
 
     useEffect(() => {
         // turned off to test without location permission in browser
@@ -45,6 +50,15 @@ function App() {
             const place = AVAILABLE_PLACES.find((place) => place.id === id);
             return [place, ...prevPickedPlaces];
         });
+
+        const storedIds =
+            JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+        if (storedIds.indexOf(id) === -1) {
+            localStorage.setItem(
+                "selectedPlaces",
+                JSON.stringify([id, ...storedIds]),
+            );
+        }
     }
 
     function handleRemovePlace() {
@@ -54,6 +68,15 @@ function App() {
             ),
         );
         modal.current.close();
+
+        const storedIds =
+            JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+        localStorage.setItem(
+            "selectedPlaces",
+            JSON.stringify(
+                storedIds.filter((id) => id !== selectedPlace.current),
+            ),
+        );
     }
 
     return (
